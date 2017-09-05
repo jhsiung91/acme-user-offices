90663152a6874b5e982b68da4736c2dbd941d1ba
Jerry Hsiung <jhsiung91@gmail.com>
2017-09-05 16:55:49 -0400

    work on routes

--- routes/offices.js
+++ routes/offices.js
@@ -1,4 +1,16 @@
 const router = require('express').Router();
 const Office = require('../db').models.Office;
 
+router.get('/', (req, res, next)=> {
+  res.send("hello offices");
+})
+
+router.post('/', (req, res, next)=> {
+  res.send("post offices");
+})
+
+router.delete('/:id', (req, res, next)=> {
+  res.send("delete offices");
+})
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,4 +2,20 @@ const router = require('express').Router();
 const User = require('../db').models.User;
 const Office = require('../db').models.Office;
 
+router.get('/', (req, res, next)=> {
+	res.send("hello users");
+})
+
+router.post('/', (req, res, next)=> {
+	res.send("post users");
+})
+
+router.delete('/:id', (req, res, next)=> {
+	res.send("delete users");
+})
+
+router.put('/:id', (req, res, next)=> {
+	res.send("put update users");
+})
+
 module.exports = router;
